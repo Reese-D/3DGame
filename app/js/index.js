efae4c27070d14cd7c722e16adeae5d83ab1d18f
Reese De Wind <Reese.DeWind@gmail.com>
2017-04-15 20:22:31 -0400

    added collision with walls and made arena smaller

--- app/js/index.js
+++ app/js/index.js
@@ -12,6 +12,8 @@ var instructions = document.getElementById( 'instructions' );
 
 // http://www.html5rocks.com/en/tutorials/pointerlock/intro/
 
+let wallDist = 250
+
 var havePointerLock = 'pointerLockElement' in document || 'mozPointerLockElement' in document || 'webkitPointerLockElement' in document;
 
 if ( havePointerLock ) {
@@ -85,7 +87,7 @@ var canJump = false;
 
 var prevTime = performance.now();
 var velocity = new THREE.Vector3();
-
+p
 function init() {
 
     camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 1, 1500 );
@@ -164,24 +166,29 @@ function init() {
     document.addEventListener( 'keyup', onKeyUp, false );
 
     raycaster = new THREE.Raycaster( new THREE.Vector3(), new THREE.Vector3( 0, - 1, 0 ), 0, 10 );
-
+    wallDist = 250
+    
     //floor
     makePlane(0, - Math.PI/2, "js/textures/floor.jpg");
     //roof
-    makePlane(500, Math.PI/2, "js/textures/wall.jpg");
+    makePlane(wallDist, Math.PI/2, "js/textures/wall.jpg");
 
     //Creates wall translated on z axis
-    makeWall(new THREE.Vector3(0,0,1), -500, 0, "js/textures/wall.jpg");
-    makeWall(new THREE.Vector3(0,0,1), 500, Math.PI, "js/textures/wall.jpg");
+    makeWall(new THREE.Vector3(0,0,1), -wallDist, 0, "js/textures/wall.jpg");
+    makeWall(new THREE.Vector3(0,0,1), wallDist, Math.PI, "js/textures/wall.jpg");
 
     //makes wall translated on x axis
-    makeWall(new THREE.Vector3(1,0,0), 500, -Math.PI/2, "js/textures/wall.jpg");
-    makeWall(new THREE.Vector3(1,0,0), -500,  Math.PI/2, "js/textures/wall.jpg");
+    makeWall(new THREE.Vector3(1,0,0), wallDist, -Math.PI/2, "js/textures/wall.jpg");
+    makeWall(new THREE.Vector3(1,0,0), -wallDist,  Math.PI/2, "js/textures/wall.jpg");
 
     //create eye
-    //makeEye(translation axis, rotation axis, translation, rotation, texture, size, vertices)
-    makeEye( new THREE.Vector3(0, 1, 0), new THREE.Vector3(0,0,0), 20, 0, "js/textures/eye2.jpg", 10, 100);
-
+    //makeEye(translation axis, rotation axis, translation, rotation, texture, size, vertices, velocity)
+    makeEye( new THREE.Vector3(1, 1, 0), new THREE.Vector3(0,0,0), 100, 0, "js/textures/eye2.jpg", 10, 20, new THREE.Vector3(-10, 140, 0));
+    makeEye( new THREE.Vector3(-1, 1, 0), new THREE.Vector3(0,0,0), 100, 0, "js/textures/eye2.jpg", 10, 20, new THREE.Vector3(10, 150,0));
+    makeEye( new THREE.Vector3(-0.5, 1, 0.5), new THREE.Vector3(0,0,0), 50, 0, "js/textures/eye2.jpg", 10, 20, new THREE.Vector3(-50, 140,0));
+    makeEye( new THREE.Vector3(0.5, 1, 0.5), new THREE.Vector3(0,0,0), 70, 0, "js/textures/eye2.jpg", 10, 20, new THREE.Vector3(-22, 40, 22));
+    makeEye( new THREE.Vector3(-1, 1, -1), new THREE.Vector3(0,0,0), 115, 0, "js/textures/eye2.jpg", 10, 20, new THREE.Vector3(10, 200, -25));
+    makeEye( new THREE.Vector3(-1, 1, 1), new THREE.Vector3(0,0,0), 200, 0, "js/textures/eye2.jpg", 10, 20, new THREE.Vector3(50, 100, 30));
 
 
     geometry = new THREE.BoxGeometry( 20, 20, 20 );
@@ -210,9 +217,10 @@ function makeWall(axis, offset, rot, tex ){
 	new THREE.MeshPhongMaterial({ map: wallTex})
     );
     wall.translateOnAxis(axis, offset);
-    wall.translateY(250);
+    wall.translateY(wallDist);
     wall.rotateY(rot);
     scene.add(wall);
+    structures.push(wall);
 }
 
 function makePlane(offset, rot, tex){
@@ -229,7 +237,7 @@ function makePlane(offset, rot, tex){
     scene.add( plane )
 }
 
-function makeEye(axis1, axis2, offset, rot, tex, radius, vertices){
+function makeEye(axis1, axis2, offset, rot, tex, radius, vertices, velocity){
     var eyeTex = new THREE.TextureLoader().load(tex, THREE.SphericalRefractionMapping);
     geometry = new THREE.SphereGeometry(radius, vertices, vertices);
     // modify UVs to accommodate MatCap texture
@@ -248,11 +256,21 @@ function makeEye(axis1, axis2, offset, rot, tex, radius, vertices){
     }
     material = new THREE.MeshPhongMaterial({map: eyeTex});
     var eyeball = new THREE.Mesh( geometry, material );
+    
+    eyeball.xRay = new THREE.Raycaster(eyeball.position, new THREE.Vector3(1,0,0), 0, radius+3);
+    eyeball.nxRay = new THREE.Raycaster(eyeball.position, new THREE.Vector3(-1,0,0), 0, radius+3);
+    eyeball.yRay = new THREE.Raycaster(eyeball.position, new THREE.Vector3(0,1,0), 0, radius+3);
+    eyeball.nyRay = new THREE.Raycaster(eyeball.position, new THREE.Vector3(0,-1,0), 0, radius+3);
+    eyeball.zRay = new THREE.Raycaster(eyeball.position, new THREE.Vector3(0,0,1), 0, radius+3);
+    eyeball.nzRay = new THREE.Raycaster(eyeball.position, new THREE.Vector3(0,0,-1), 0, radius+3);
+    
     eyeball.overdraw = true;
     eyeball.castShadow = true;
     eyeball.translateOnAxis(axis1, offset);
     eyeball.rotateOnAxis(axis2, rot);
     scene.add( eyeball );
+    eyeball.velocity = velocity
+    eyeball.bounce = velocity.y;
     objects.push(eyeball);
 }
 
@@ -268,6 +286,30 @@ function onWindowResize() {
 
 }
 
+function spheresIntersect(sphere1, sphere2){
+
+    var radiiSum = sphere1.geometry.boundingSphere.radius + sphere2.geometry.boundingSphere.radius;
+    var center1 = sphere1.geometry.boundingSphere.center;
+    var center2 = sphere2.geometry.boundingSphere.center;
+    if(center1.distanceTo(center2) < radiiSum){
+	return true;
+    }
+    return false;
+}
+
+function vectorMax(v){
+    return Math.max(v.x, v.y, v.z);
+}
+
+function vectorMin(v){
+    return Math.min(v.x, v.y, v.z);
+}
+
+function normalizedVector(v){
+    var max = Math.abs(vectorMax(v));
+    return new THREE.Vector3(v.x / max, v.y / max, v.z/max);
+}
+
 function animate() {
 
     requestAnimationFrame( animate );
@@ -276,8 +318,18 @@ function animate() {
 	raycaster.ray.origin.copy( controls.getObject().position );
 	raycaster.ray.origin.y -= 10;
 
-	var intersections = raycaster.intersectObjects( objects );
+	for(var i = 0; i < objects.length; i++){
+	    for(var j = i; j < objects.length; j++){
+		if(i == j){
+		    continue;
+		}
+		//TODO: if spheresIntersect deflect
+	    }
+	}
 
+	
+	var intersections = raycaster.intersectObjects( objects );
+	//console.log(objects[0]);
 	var isOnObject = intersections.length > 0;
 
 	var time = performance.now();
@@ -304,11 +356,75 @@ function animate() {
 	controls.getObject().translateY( velocity.y * delta );
 	controls.getObject().translateZ( velocity.z * delta );
 
+	for(var i = 0; i < objects.length; i++){
+	    objects[i].velocity.y -= 9.8 * 10.0 * delta;
+	    //objects[i].velocity.y -= 1;
+	    //console.log(9.8 * 100.0 * delta);
+	    objects[i].translateX( objects[i].velocity.x * delta );
+	    objects[i].translateY( objects[i].velocity.y * delta );
+	    objects[i].translateZ( objects[i].velocity.z * delta );
+	    //console.log(objects[i].position.y);
+	    var radius = objects[i].geometry.boundingSphere.radius;
+	    if(objects[i].position.y - radius <= 0){
+	     	objects[i].velocity.y = objects[i].bounce;
+	    }
+	    var currObj = objects.splice(i, 1)[0];
+	    	    
+	    // if(currObj.zRay.intersectObjects(objects).length > 0 || currObj.zRay.intersectObjects(structures).length > 0){
+	    // 	currObj.velocity.z *= -1;
+	    // 	currObj.position.z -= 1;
+	    // }
+	    
+	    
+	    // if(currObj.nzRay.intersectObjects(objects).length > 0 || currObj.nzRay.intersectObjects(structures).length > 0){
+	    // 	currObj.velocity.z *= -1;
+	    // 	currObj.position.z += 1;
+	    // }
+	    
+	    // if(currObj.yRay.intersectObjects(objects).length > 0 || currObj.yRay.intersectObjects(structures).length > 0){
+	    // 	currObj.velocity.y *= -1;
+	    // 	currObj.position.y -= 1;
+	    // }
+
+	    
+	    // if(currObj.nyRay.intersectObjects(objects).length > 0 || currObj.nyRay.intersectObjects(structures).length > 0){
+	    // 	currObj.velocity.y *= -1;
+	    // 	currObj.position.y += 1;
+	    // }
+
+	    
+	    // if(currObj.xRay.intersectObjects(objects).length > 0 || currObj.xRay.intersectObjects(structures).length > 0){
+	    // 	currObj.velocity.x *= -1;
+	    // 	currObj.position.x -= 1;
+	    // }
+
+	    
+	    // if(currObj.nxRay.intersectObjects(objects).length > 0 || currObj.nxRay.intersectObjects(structures).length > 0){
+	    // 	currObj.velocity.x *= -1;
+	    // 	currObj.position.x += 1;
+	    //}
+	    if(currObj.position.x + radius > wallDist || currObj.position.x - radius < -wallDist){
+		currObj.velocity.x *= -1
+	    }
+	    if(currObj.position.y + radius > wallDist || currObj.position.y - radius < -wallDist){
+		currObj.velocity.y *= -1
+	    }
+	    if(currObj.position.z + radius > wallDist || currObj.position.z - radius < -wallDist){
+		currObj.velocity.z *= -1
+	    }
+	    objects.splice(i, 0, currObj);
+	    //var objectRay = THREE.Raycaster(objects[i].position, normalizedVector(objects[i].velocity), 0, radius+1);
+	    //var collisions = 
+	    //var diff = new THREE.Vector3(100, 200, 300).sub(new THREE.Vector3(100, 210, 290));
+	    //console.log(diff);
+
+	}
+	
 	if ( controls.getObject().position.y < 10 ) {
 
 	    velocity.y = 0;
 	    controls.getObject().position.y = 10;
-
+	    
 	    canJump = true;
 
 	}
